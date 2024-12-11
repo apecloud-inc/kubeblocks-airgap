d9d68ea28f9e7d0fbab0ed787fe018059dad3d68
huangzhangshu <huangzhangshu@apecloud.com>
2024-12-11 16:34:41 +0800

    chore: change vastbase download from ent

--- .github/scripts/save_charts.sh
+++ .github/scripts/save_charts.sh
@@ -261,7 +261,9 @@ tar_charts_package() {
             chart_version=${chart#*:}
 
             case "$chart_tmp" in
-                "kubeblocks-cloud"*|"clickhouse"*|"damengdb"*|"elasticsearch"*|"gaussdb"*|"gbase"*|"kafka"*|"kingbase"*|"loki"*|"minio"*|"mssql"*|"mysql"*|"oceanbase"*|"postgresql"*|"qdrant"*|"redis"*|"starrocks"*|"victoria-metrics"*)
+                "kubeblocks-cloud"*|"clickhouse"*|"damengdb"*|"elasticsearch"*|"gaussdb"*|"gbase"*|\
+                "kafka"*|"kingbase"*|"loki"*|"minio"*|"mssql"*|"mysql"*|"oceanbase"*|"postgresql"*|\
+                "qdrant"*|"redis"*|"starrocks"*|"victoria-metrics"*|"vastbase"*)
                     helm repo add ${ENT_REPO_NAME} --username ${CHART_ACCESS_USER} --password ${CHART_ACCESS_TOKEN} ${KB_ENT_REPO_URL}
                     helm repo update ${ENT_REPO_NAME}
                     ent_flag=1
