1b357adb1681e391769ac958620d337686a248c9
huangzhangshu <109708205+JashBook@users.noreply.github.com>
2025-06-24 11:48:57 +0800

    chore: add scripts to release charts (#94)
    (cherry picked from commit fa18e48b215de5e92612b7d580243755ecfd9cb5)

---
+++ cloud/scripts/generate_image_list_arm64.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+DIRECTORY="kb-charts/kubeblocks-image-list"
+FILES_TO_READ=("kubeblocks-enterprise.txt" "ape-local-csi-driver.txt" "cert-manager.txt" "clickhouse.txt" "damengdb-arm.txt" "elasticsearch-arm.txt" "ingress-nginx.txt" "kafka.txt" "gaussdb.txt" "influxdb.txt" "kingbase-images" "kubebench.txt" "metallb.txt" "metrics-server.txt" "minio.txt" "mongodb.txt" "mssql.txt" "mysql-arm.txt" "oceanbase.txt" "postgres-operator.txt" "postgresql.txt" "qdrant.txt" "rabbitmq.txt" "redis.txt" "rocketmq-images.txt" "starrocks-images.txt" "tidb.txt" "vastbase.txt" "zookeeper-images.txt" "victoria-metrics.txt" "spiderpool.txt"  )
+OUTPUT_FILE="kubeblocks-image-list.txt"
+> "$OUTPUT_FILE"
+for file_name in "${FILES_TO_READ[@]}"; do
+    file_path="$DIRECTORY/$file_name"
+    if [ ! -f "$file_path" ]; then
+        echo "File $file_name not found in the directory."
+        exit 1
+    fi
+    echo "Appending $file_name to $OUTPUT_FILE"
+    cat "$file_path" >> "$OUTPUT_FILE"
+done
