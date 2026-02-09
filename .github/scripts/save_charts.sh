3d841bc3b71038826361a3c7c49ec5bf9e1164e3
huangzhangshu <huangzhangshu@apecloud.com>
2026-02-09 17:25:56 +0800

    chore: fix save gbase

--- .github/scripts/save_charts.sh
+++ .github/scripts/save_charts.sh
@@ -382,7 +382,7 @@ tar_charts_package() {
             case "$chart_tmp" in
                 "kubeblocks-cloud"*|"ape-local-csi-driver"*|"damengdb"*|"gaussdb"*|\
                 "kingbase"*|"mssql"*|"oceanbase"*|"starrocks"*|"vastbase"*|"goldendb"*|"tdsql"*|\
-                "oracle"*|"greatdb"*|"doris"*|"hadoop"*|"hive"*|"nacos"*)
+                "oracle"*|"greatdb"*|"doris"*|"hadoop"*|"hive"*|"nacos"*|"gbase"*)
                     helm repo add ${ENT_REPO_NAME} --username ${CHART_ACCESS_USER} --password ${CHART_ACCESS_TOKEN} ${KB_ENT_REPO_URL}
                     helm repo update ${ENT_REPO_NAME}
                     ent_flag=1
